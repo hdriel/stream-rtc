74b3ebebf5b4637af9cef7b12863f1ef4d30fd9f
Hadriel Benjo <hdriel@gmail.com>
2025-09-16 20:03:59 +0300

    update scripts

--- demo/src/utils/elements.ts
+++ demo/src/utils/elements.ts
@@ -10,11 +10,8 @@ const actionsElement = document.querySelector('#actions') as HTMLDivElement;
 const videosElement = document.querySelector('#videos') as HTMLVideoElement;
 export const callButtonElement = document.querySelector('#call') as HTMLVideoElement;
 export const hangupButtonElement = document.querySelector('#hangup') as HTMLVideoElement;
-
 const answerEl = document.querySelector('#answer');
-
 const title = document.querySelector('#use-case') as Element;
-
 export const joinRoomElement = document.querySelector('#join-room') as Element;
 
 export function scenario(text: string) {
